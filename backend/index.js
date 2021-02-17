fa6b8a6a18cb5a2530d3394c803b0a19e91758fd
Oleh <oleh.matvii@bitsorchestra.com>
2021-02-17 22:00:34 +0200

    bh

--- backend/index.js
+++ backend/index.js
@@ -4,3 +4,12 @@ const app = express();
 app.get('/test', function(req, res) {
    return res.send('hello')
 })
+
+app.listen(5000, function (err) {
+    if (err) {
+        console.log(err)
+        return throw Error('')
+    }
+    console.log('Server has been started')
+
+})
